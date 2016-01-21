da91fdcf742dd2ae05130ac5205a74e2eec4e21d
Christina <christinajeanplatt@gmail.com>
2016-01-20 22:25:32 -0500

    hexagons are working but not spawning correctly

--- js/main.js
+++ js/main.js
@@ -1,30 +1,56 @@
 $( document ).ready(function() {
     console.log( "ready!" );
-    $( "body" ).css( "height", function( index ) {
-	  var windowHeight = window.innerHeight;
-	  console.log(windowHeight);
-	  return windowHeight
-	});
+    $( "body" ).css( "height", window.innerHeight);
 
     $.fn.hexify = function() {
-    	$(this).append($('<div>').addClass('hextop'));
-    	$(this).append($('<div>').addClass('hexmiddle'));
-    	$(this).append($('<div>').addClass('hexbottom'));
+    	$( this ).append($('<div>').addClass('hextop'));
+    	$( this ).append($('<div>').addClass('hexmiddle'));
+    	$( this ).append($('<div>').addClass('hexbottom'));
     	return this;
 	}
 
+	$.fn.animatesShapes = function(toggle) {
 
-	
-	$('.hexagon').each(function( index, element ) {
-	    var time = Math.floor(5000 * Math.random());
-	    var xcoordstart = window.innerHeight * Math.random();
-	    var xcoordend = window.innerHeight * Math.random();
-	    $( element ).hexify().offset({top:0,left:xcoordstart}).animate({
-			opacity:0.25,
-			left: xcoordend,
-			top: "+=500"
-		},time, "linear");    
-	  });
+		var time = 2500 + Math.floor(2500 * Math.random());
+
+    	$( this ).css("opacity",1);
+
+		if ( toggle ) {
+			var yCoordEnd = "+=80%";
+		} else {
+			yCoordEnd = "-=80%";
+		}		
+
+		$( this ).animate({
+			opacity: 0.25,
+			left: window.innerHeight * Math.random(),
+			top: yCoordEnd
+		},time, "linear", function(){
+			toggle = !toggle;
+			$(this).animatesShapes(toggle);
+		});
+
+		return this
+	}
+
+	function spawn(numberOfHexagons, totalHexagons) {
+	    $( 'body' ).append($('<div>').addClass('hexagon'));
+	    console.log(numberOfHexagons);
+	    numberOfHexagons++;
+	    if( numberOfHexagons < totalHexagons ){
+	        setTimeout( spawn(numberOfHexagons, totalHexagons), 3000 );
+	    }
+	}
+	spawn(0,10);
+
+
+
+	$('.hexagon').hexify().each(function( index, hexagon ) {
+	    // var xCoordStart = window.innerHeight * Math.random();
+	    $( hexagon ).offset({top:-200, left:window.innerHeight * Math.random()})
+	    var atTop=true;
+	    $( this).animatesShapes(atTop);	    
+	});
 
     
 
