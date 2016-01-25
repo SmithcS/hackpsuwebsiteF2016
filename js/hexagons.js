2cf320ec03aa5128557f5339e170fc2a43deffe1
Christina <christinajeanplatt@gmail.com>
2016-01-25 17:12:03 -0500

    Hexagons are responsive" ;

--- js/hexagons.js
+++ js/hexagons.js
@@ -1,5 +1,9 @@
- // array with white, dark blue, light blue and yellow
+function animations(){
+
+	// array with white, dark blue, light blue and yellow
     selectColor = ['#FFFFFF','#2A202F','#0099FF','#e2cb59']
+
+
     // adds divs to create a hexagon
     $.fn.hexify = function(size) {
     	var randomColor = selectColor[Math.floor(Math.random()*4)];
@@ -25,49 +29,77 @@
     	return this;
 	}
 
-	$.fn.animatesShapes = function(toggle,size) {
+	// finds a random x coordinate that falls within the width of the window
+	$.fn.getXCoord = function() {
+		if ( $( this ).width() == 0 ) {
+			var ErrorCatchWidth = 156;
+		} else {
+			ErrorCatchWidth = 0;
+		}
 
+		var xCoord=Math.random();
+
+		if ( ( xCoord * window.innerWidth + $( this ).width() * 1.5 + ErrorCatchWidth ) > window.innerWidth ) {
+			xCoord = window.innerWidth - $( this ).width() * 1.5 - ErrorCatchWidth * 1.5;
+		} else {
+			xCoord = xCoord * window.innerWidth;
+		}
+
+		return xCoord
+	}
+
+	$.fn.animatesShapes = function() {
+    	size = $(this).prop('size');
 		// duration of animation may be altered depending on effect to achieve
-		var time = 2500 * size + Math.floor(1500 * size * Math.random());
-
+		time = (2500 + Math.floor(1500 * Math.random()) ) * size;
+		currentPositionOfShape = $( this ).offset();
 
 		//reset opacity
-    	$( this ).css("opacity", 1 / size);
+    	$( this ).css("opacity", 1 / size).offset({
+    		top: currentPositionOfShape.top,
+    		left: $( this ).getXCoord()
+    	});
 
     	// toggle variable decides whether it's going up or down
-		if ( toggle ) {
-			var yCoordEnd = "+=120%";
+		if ( currentPositionOfShape.top < 0.5 * window.innerHeight ) {
+			var yCoordEnd = window.innerHeight;
 		} else {
-			yCoordEnd = "-=120%";
-		}		
+			yCoordEnd = -100;
+		}
 
 		// simple animation function, sets new coordinates, toggles, and recurses
 		$( this ).animate({
 			opacity: (1 / size) - 0.2 * size,
-			left: window.innerWidth * Math.random() - size * 52,
+			left: $( this ).getXCoord(),
 			top: yCoordEnd
 		},time, "linear", function(){
-			toggle = !toggle;
-			$(this).animatesShapes(toggle, size);
+			$( this ).animatesShapes();
 		});
 
 		return this
 	}	
 
-	var numberOfHexagons = 0;
-	
+	function reanimate() {
+		$('.hexagon').each( function( index ) {
+			$(this).animatesShapes();
+		});
+	}
+
 	//spawns shapes, shocker
-	(function spawn() {
-		var atTop=true;
+	function spawn() {
 		// timeout function for creating a hexagon on an interval
 	    setTimeout(function() {
 	    	var size = numberOfHexagons % 3 + 1;
 	        $( 'body' ).prepend($('<div>')
 	        	.addClass('hexagon')
+	        	.prop('size', size)
 	        	.hexify(size)
-	        	.css('z-index', -1 * size)
-	        	.offset({top:-50, left:window.innerWidth * Math.random() - size * 52})
-	        	.animatesShapes(atTop, size));
+	        	.offset({
+	        		left: 0,
+	        		top: -150
+	        	})
+	        	.css( 'z-index', -1 * size )
+	        	.animatesShapes());
 
 	        numberOfHexagons++;
 	        if (numberOfHexagons < Math.floor(window.innerWidth/40)) {
@@ -76,4 +108,26 @@
 
 	    }, 1000);
 
-	})(); 
+	};
+
+	$( window ).resize(function() {
+
+		$( '.hexagon' ).stop();
+
+		$( '.hexagon' ).each( function( index ) {
+			var currentPositionOfShape = $( this ).offset();
+
+	 		if ( (currentPositionOfShape.left + $( this ).width() * 1.5) >= window.innerWidth ) {
+				$( this ).remove();
+			}
+
+			$( "body" ).css( "width", window.innerWidth);
+
+		});
+
+		reanimate();
+	});
+
+	numberOfHexagons = 0;
+	spawn(); 
+}
--- js/main.js
+++ js/main.js
@@ -2,4 +2,22 @@ $( document ).ready(function() {
     console.log( "ready!" );
     $( "#content" ).css( "height", window.innerHeight);
 
+    var isMobile = true;
+
+	if ( screen.width > 700 ) {
+		isMobile = false;
+	} else {
+		isMobile = true;
+	}
+
+	if(isMobile) {
+		
+	 
+	} else {
+		animations();
+
+
+	}
+    
+
 });
