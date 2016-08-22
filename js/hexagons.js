6a174b75e9b9f32e7a851dad13026e37e90dc1f7
Smith Sopp <smithsopp@Smiths-MacBook-Pro.local>
2016-08-22 18:11:45 -0500

    new files lol

--- js/hexagons.js
+++ js/hexagons.js
@@ -1,12 +1,39 @@
 function animations(){
 
-	// array with white, dark blue, light blue and yellow
-    selectColor = ['#FFFFFF','#2A202F','#0099FF','#e2cb59']
+	// array with white, dark blue, light blue, yellow and coral
+    selectColor = ['#FFFFFF','#000033','#3399CC','#FFCC66','#ff9966']
+
+
+    //spawns shapes, shocker
+	function spawn() {
+		// timeout function for creating a hexagon on an interval
+	    setTimeout(function() {
+	    	var size = numberOfHexagons % 3 + 1;
+	        $( '#header' ).prepend($('<div>')
+	        	.addClass('hexagon')
+	        	.prop('size', size)
+	        	.hexify(size)
+	        	.offset({
+	        		left: 0,
+	        		top: -150
+	        	})
+	        	.css( 'z-index', -1 * size )
+	        	.animatesShapes());
+
+	        numberOfHexagons++;
+	        if (numberOfHexagons < Math.floor(window.innerWidth/80)) {
+	            spawn();
+	        }
+
+	    }, 100);
+
+	};
 
 
     // adds divs to create a hexagon
     $.fn.hexify = function(size) {
-    	var randomColor = selectColor[Math.floor(Math.random()*4)];
+    	var randomColor = selectColor[Math.floor(Math.random()*5)];
+    	
     	$( this ).append($('<div>').addClass('hextop').css({
     		'border-bottom-color': randomColor,
     		'border-bottom-width': 15 * size,
@@ -29,6 +56,37 @@ function animations(){
     	return this;
 	}
 
+	$.fn.animatesShapes = function() {
+    	size = $(this).prop('size');
+		// duration of animation may be altered depending on effect to achieve
+		time = (2500 + Math.floor(1500 * Math.random()) ) * size;
+		currentPositionOfShape = $( this ).offset();
+
+		//reset
+    	$( this ).css("opacity", 1 / size).offset({
+    		top: currentPositionOfShape.top,
+    		left: $( this ).getXCoord()
+    	});
+
+    	// toggle variable decides whether it's going up or down
+		if ( currentPositionOfShape.top < 0.5 * window.innerHeight ) {
+			var yCoordEnd = window.innerHeight *1.5;
+		} else {
+			yCoordEnd = -100;
+		}
+
+		// simple animation function, sets new coordinates, toggles, and recurses
+		$( this ).animate({
+			opacity: (1 / size) - 0.2 * size,
+			left: $( this ).getXCoord(),
+			top: yCoordEnd
+		},time, "linear", function(){
+			$( this ).animatesShapes();
+		});
+
+		return this
+	}	
+
 	// finds a random x coordinate that falls within the width of the window
 	$.fn.getXCoord = function() {
 		if ( $( this ).width() == 0 ) {
@@ -48,36 +106,11 @@ function animations(){
 		return xCoord
 	}
 
-	$.fn.animatesShapes = function() {
-    	size = $(this).prop('size');
-		// duration of animation may be altered depending on effect to achieve
-		time = (2500 + Math.floor(1500 * Math.random()) ) * size;
-		currentPositionOfShape = $( this ).offset();
-
-		//reset opacity
-    	$( this ).css("opacity", 1 / size).offset({
-    		top: currentPositionOfShape.top,
-    		left: $( this ).getXCoord()
-    	});
-
-    	// toggle variable decides whether it's going up or down
-		if ( currentPositionOfShape.top < 0.5 * window.innerHeight ) {
-			var yCoordEnd = window.innerHeight;
-		} else {
-			yCoordEnd = -100;
-		}
-
-		// simple animation function, sets new coordinates, toggles, and recurses
-		$( this ).animate({
-			opacity: (1 / size) - 0.2 * size,
-			left: $( this ).getXCoord(),
-			top: yCoordEnd
-		},time, "linear", function(){
-			$( this ).animatesShapes();
-		});
-
-		return this
-	}	
+	$( window ).resize(function() {
+		$( '.hexagon' ).stop();
+		$( "body" ).css( "width", window.innerWidth);
+		reanimate();
+	});
 
 	function reanimate() {
 		$('.hexagon').each( function( index ) {
@@ -85,48 +118,38 @@ function animations(){
 		});
 	}
 
-	//spawns shapes, shocker
-	function spawn() {
-		// timeout function for creating a hexagon on an interval
-	    setTimeout(function() {
-	    	var size = numberOfHexagons % 3 + 1;
-	        $( 'body' ).prepend($('<div>')
-	        	.addClass('hexagon')
-	        	.prop('size', size)
-	        	.hexify(size)
-	        	.offset({
-	        		left: 0,
-	        		top: -150
-	        	})
-	        	.css( 'z-index', -1 * size )
-	        	.animatesShapes());
 
-	        numberOfHexagons++;
-	        if (numberOfHexagons < Math.floor(window.innerWidth/40)) {
-	            spawn();
-	        }
+	// var checker, test = $('#about').offset();
+	// if (top > this.top) {
+	// 	var inframe = false;
+	// } else {
+	// 	inframe = true;
+	// }
+	
 
-	    }, 1000);
+	// $(window).scroll( function() {
+	// 	var top = $(this).scrollTop();
 
-	};
+	// 	if ( top > test.top ) {
+	// 		inframe = false;
+	// 	} else {
+	// 		inframe = true;
+	// 	}
+	// 	console.log(inframe)
 
-	$( window ).resize(function() {
+	// 	if (checker != inframe) {
+	// 		if (inframe) {
+	// 			reanimate();
+	// 		} 
+	// 		if (!inframe) {
+	// 			console.log('poop')
+	// 			$('.hexagon').stop();
+	// 		}
+	// 	}
 
-		$( '.hexagon' ).stop();
+	// 	checker = inframe;
 
-		$( '.hexagon' ).each( function( index ) {
-			var currentPositionOfShape = $( this ).offset();
-
-	 		if ( (currentPositionOfShape.left + $( this ).width() * 1.5) >= window.innerWidth ) {
-				$( this ).remove();
-			}
-
-			$( "body" ).css( "width", window.innerWidth);
-
-		});
-
-		reanimate();
-	});
+	// });
 
 	numberOfHexagons = 0;
 	spawn(); 
