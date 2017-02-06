6a68232e72c82ac5062cdf8ba5e579b630699297
Scott Miller <sam6258@psu.edu>
2017-02-06 13:53:48 -0500

    Add folders to gitignore

--- hackpsu17/platforms/android/assets/www/cordova_plugins.js
+++ hackpsu17/platforms/android/assets/www/cordova_plugins.js
@@ -1,9 +1,20 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "id": "phonegap-plugin-push.PushNotification",
+        "file": "plugins/phonegap-plugin-push/www/push.js",
+        "pluginId": "phonegap-plugin-push",
+        "clobbers": [
+            "PushNotification"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.3.1"
+    "cordova-plugin-whitelist": "1.3.1",
+    "phonegap-plugin-push": "1.9.2",
+    "cordova-plugin-cocoapod-support": "1.2.14"
 };
 // BOTTOM OF METADATA
 });
--- hackpsu17/platforms/browser/platform_www/cordova_plugins.js
+++ hackpsu17/platforms/browser/platform_www/cordova_plugins.js
@@ -1,9 +1,28 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/phonegap-plugin-push/www/push.js",
+        "id": "phonegap-plugin-push.PushNotification",
+        "pluginId": "phonegap-plugin-push",
+        "clobbers": [
+            "PushNotification"
+        ]
+    },
+    {
+        "file": "plugins/phonegap-plugin-push/www/browser/push.js",
+        "id": "phonegap-plugin-push.BrowserPush",
+        "pluginId": "phonegap-plugin-push",
+        "clobbers": [
+            "PushNotification"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.3.1"
+    "cordova-plugin-whitelist": "1.3.1",
+    "phonegap-plugin-push": "1.9.2",
+    "cordova-plugin-cocoapod-support": "1.2.14"
 }
 // BOTTOM OF METADATA
 });
