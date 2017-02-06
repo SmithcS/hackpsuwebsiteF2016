b7c7370ad07f5bddc5c6cefc1789165d1209f704
Scott Miller <sam6258@psu.edu>
2017-02-06 14:17:56 -0500

    Add phone gap code

--- hackpsu17/www/js/index.js
+++ hackpsu17/www/js/index.js
@@ -28,6 +28,38 @@ var app = {
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
         this.receivedEvent('deviceready');
+	app.push = PushNotification.init({
+	     	"android": {
+		 	"senderID": "838368032544"
+	     	},
+	     	"ios": {
+	       		"sound": true,
+	       		"vibration": true,
+	       		"badge": true
+	     	},
+		"windows": {}
+	});
+
+	app.push.on('registration', function(data) {
+	     	console.log("registration event: " + data.registrationId);
+	     	document.getElementById("regId").innerHTML = data.registrationId;
+	     	var oldRegId = localStorage.getItem('registrationId');
+	     	if (oldRegId !== data.registrationId) {
+	 		// Save new registration ID
+	 		localStorage.setItem('registrationId', data.registrationId);
+			// Post registrationId to your app server as the value has changed
+ 		}
+	});
+
+ 	app.push.on('error', function(e) {
+	     	console.log("push error = " + e.message);
+	});
+
+	app.push.on('notification', function(data) {
+		console.log("notification received";
+		console.log("data" + data.message);		
+		console.log("addtional data" + data.additionalData.foreground);
+	});
     },
 
     // Update DOM on a Received Event
