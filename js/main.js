a443a76b93ad5e1f4271c2b7416714c634370800
Sushrut Shringarputale <sush.shring@gmail.com>
2017-02-12 19:40:57 -0500

    Fixed css styling on phones. Added media queries that allow phones to properly render the sponsor hexagons without having to scroll for miles.

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    console.log("ready!");
+
+    var isMobile = false;
+    if (window.innerWidth < 600)
+      isMobile = true;
+
+    var pastSponsors = Info["past_sponsors"];
+    console.log(pastSponsors);
+
+    // $('#sponsors').child
+});
